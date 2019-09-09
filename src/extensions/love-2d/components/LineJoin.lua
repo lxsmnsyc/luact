ef0fcb24311bfcc84f20635a66d1ded5475f03e2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-09-09 10:16:37 +0800

    Create LineJoin.lua

---
+++ src/extensions/love-2d/components/LineJoin.lua
@@ -0,0 +1,61 @@
+--[[
+    Luact
+	
+    MIT License
+    Copyright (c) 2019 Alexis Munsayac
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+--]]
+local Component = require "luact.src.component"
+
+local Context = require "luact.src.components.context"
+
+local typeLineJoin = require "luact.src.extensions.love-2d.types.linejoin"
+local typeOptional = require "luact.src.types.optional"
+
+local LineJoinContext = Context.new("none")
+
+local function LineJoin(props)
+  local value = props.value
+
+  local parentValue = LineJoinContext:use()
+  
+  return {
+    beforeDraw = function ()
+      love.graphics.setLineJoin(value)
+    end,
+    Context.Provider {
+      context = LineJoinContext,
+      value = value,
+      children = props.children,
+    },
+    afterDraw = function ()
+      love.graphics.setLineJoin(parentValue)
+    end
+  }
+end
+
+local optionalLineJoin = typeOptional(typeLineJoin)
+
+local propTypes = {
+  value = optionalLineJoin
+}
+
+local defaultProps = {
+  value = "none"
+}
+
+return Component("love.LineJoin", LineJoin, propTypes, defaultProps)
