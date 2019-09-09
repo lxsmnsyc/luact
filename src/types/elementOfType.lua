0ba80b009d06ac654106795455c34faa3851cb13
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-09-09 10:24:48 +0800

    Create elementOfType.lua

---
+++ src/types/elementOfType.lua
@@ -0,0 +1,31 @@
+--[[
+    Luact
+	
+    MIT License
+    Copyright (c) 2019 Alexis Munsayac
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+--]]
+local component = require "luact.src.types.component"
+local element = require "luact.src.types.element"
+local ELEMENT = require "luact.src.meta.ELEMENT"
+
+return function (comp)
+  assert(component(comp), "luact.types.elementOfType: comp must be a luact.component")
+  return function (el)
+    return element(el) and ELEMENT[el] == comp
+  end
+end
