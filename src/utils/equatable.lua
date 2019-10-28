8d7b99eab2d681e2b626bc4c4ad898001a030e25
Alexis Munsayac <alexis@lyon.com.ph>
2019-10-28 18:10:06 +0800

    Create equatable.lua

---
+++ src/utils/equatable.lua
@@ -0,0 +1,46 @@
+--[[
+    Luact
+	
+    MIT License
+    Copyright (c) 2019 Alexis Munsayac
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+--]]
+local mt = {
+  __eq = function (a, b)
+    local checked = {}
+    for k, v in pairs(a) do
+      local t = b[k]
+      checked[k] = true
+      if (v ~= t) then
+        return false
+      end
+    end
+    for k, v in pairs(b) do
+      if (not checked[k]) then
+        local t = a[k]
+        if (v ~= t) then
+          return false
+        end
+      end
+    end
+    return true
+  end,
+}
+
+return function (t)
+  return setmetatable(t, mt)
+end
