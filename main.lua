cf25481f7fe85fbbce43e0cdbf27c342887b5a03
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-13 16:04:00 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,23 +2,33 @@ local luact = require "luact"
 local timeout = require "timers.timeout"
 local frame = require "timers.frame"
 local idle = require "timers.init"
+local logs = require "luact.utils.logs"
 
+-- Render/Reconciler engine
 local System = luact.init({
   create_instance = function (self, constructor, props)
     return {
       constructor = constructor,
-      props = props
+      props = props,
+      children = {},
     }
   end,
   append_child = function (self, parent, child)
-    table.insert(parent, child)
+    table.insert(parent.children, child)
   end,
   commit_update = function (self, instance, old_props, new_props)
     instance.props = new_props
   end,
+  remove_child = function (self, parent, child)
+    for k, v in pairs(parent.children) do
+      if (v == child) then
+        table.remove(parent.children, k)
+      end
+    end
+  end,
   schedule = function (self, callback)
     idle.request(function (deadline)
-      callback(deadline.timeRemaining())
+      callback(deadline.timeRemaining)
     end)
   end,
 })
@@ -28,43 +38,42 @@ function love.update(dt)
   frame.update(dt)
 end
 
-local test = {}
+local test = { children = { }}
 
-local App = System.component(function ()
-  local state, set_state = luact.use_state("Loading")
+local LifecycleDemo = System.component(function ()
+  luact.use_layout_effect(function ()
+    return function ()
+      error("Unmounted!")
+    end
+  end, {})
+
+  return System.Element("Hello World", { })
+end)
+
+local Example = System.component(function ()
+  local state, set_state = luact.use_state(true)
 
   timeout.request(function ()
-    set_state("Success")
-  end, 1000)
+    set_state(false)
+  end, 1000 + 1000 * math.random())
 
+  if (state) then
+    return LifecycleDemo {}
+  end
+  return nil
+end)
+
+local App = System.basic(function ()
   return System.Fragment {
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
-    System.Element("Hello", { test = state }),
+    Example {},
+    Example {},
+    Example {},
+    Example {},
   }
 end)
+
 System.render(App {}, test)
 
-local function log(node, level)
-	local s = "{"
-
-  for k, v in pairs(node) do
-		local result = ""
-		if (type(v) == "table") then
-			result = result..log(v, level + 1)
-    else
-			result = type(v).."("..tostring(v)..")"
-		end
-		s = s.."\n"..string.rep("  ", level + 1)..tostring(k)..": "..result..","
-	end
-	s = s.."\n"..string.rep("  ", level).."}"
-	return s
-end
-
 function love.draw()
-  love.graphics.print(log(test, 0))
+  love.graphics.print(logs(test, 0))
 end
