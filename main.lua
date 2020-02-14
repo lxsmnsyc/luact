9843abafad344e42ba355f90be8cab61f04b54fc
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-14 23:40:50 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,73 +1,88 @@
-local luact = require "luact"
-local timeout = require "luact.timers.timeout"
-local logs = require "luact.utils.logs"
+local LuactLove = require "luact-love"
+local Love = require "luact-love.reconciler"
+local Luact = require "luact"
+local frame = require "luact.timers.frame"
 
--- Render/Reconciler engine
-local System = luact.init({
-  create_instance = function (self, constructor, props)
-    return {
-      constructor = constructor,
-      props = props,
-      children = {},
-    }
-  end,
-  append_child = function (self, parent, child, index)
-    parent.children[index] = child
-  end,
-  commit_update = function (self, instance, old_props, new_props)
-    instance.props = new_props
-  end,
-  remove_child = function (self, parent, child, index)
-    parent.children[index] = nil
-  end,
-})
+local Rectangle = require "luact-love.components.rectangle"
+local Color = require "luact-love.components.color"
+local Translate = require "luact-love.components.translate"
 
-function love.update(dt)
-  luact.update_frame(dt)
-end
+local ColorBox = Love.component(function (props)
+  local state, set_state = Luact.use_state(0)
 
-local test = { children = { }}
+  Luact.use_layout_effect(function ()
+    local request
 
-local LifecycleDemo = System.component(function (props)
-  luact.use_layout_effect(function ()
+    local function animate(dt)
+      set_state(function (current)
+        if (current < 1000) then
+          return current + dt
+        end
+        return dt
+      end)
+      request = frame.request(animate)
+    end
+
+    request = frame.request(animate)
+
     return function ()
-      error("Unmounted!")
+      frame.clear(request)
     end
   end, {})
 
-  return System.Element("Hello World", { key = props.key })
-end)
-
-
-local Example = System.component(function (props)
-  local state, set_state = luact.use_state(true)
-
-  luact.use_layout_effect(function ()
-    timeout.request(function ()
-      set_state(not state)
-    end, 1000 + 1000 * math.random())
-  end, {state})
-
-  if (state) then
-    return LifecycleDemo { key = props.key }
-  end
-  return nil
-end)
-
-local App = System.basic(function ()
-  return System.Fragment {
-    Example { key = 1 },
-    Example { key = 2 },
-    Example { key = 3 },
-    Example { key = 4 },
-    Example { key = 5 },
-    Example { key = 6 },
-    Example { key = 7 },
+  return Color {
+    r = state / 1000,
+    g = props.x / 100,
+    b = props.y / 100,
+    children = {
+      Rectangle {
+        mode = "fill",
+        x = props.x,
+        y = props.y,
+        width = 10,
+        height = 10,
+      },
+    }
   }
 end)
 
-System.render(App {}, test)
+local ColorColumn = Love.basic(function (props)
+  return Love.Fragment {
+    ColorBox { x = props.x, y = 10 },
+    ColorBox { x = props.x, y = 20 },
+    ColorBox { x = props.x, y = 30 },
+    ColorBox { x = props.x, y = 40 },
+    ColorBox { x = props.x, y = 50 },
+    ColorBox { x = props.x, y = 60 },
+    ColorBox { x = props.x, y = 70 },
+    ColorBox { x = props.x, y = 80 },
+    ColorBox { x = props.x, y = 90 },
+    ColorBox { x = props.x, y = 100 },
+  }
+end)
 
-function love.draw()
-  love.graphics.print(logs(test, 0))
-end
+local App = Love.basic(function (props)
+  return Translate {
+    x = props.x,
+    y = props.y,
+    children = {
+      ColorColumn { x = 10 },
+      ColorColumn { x = 20 },
+      ColorColumn { x = 30 },
+      ColorColumn { x = 40 },
+      ColorColumn { x = 50 },
+      ColorColumn { x = 60 },
+      ColorColumn { x = 70 },
+      ColorColumn { x = 80 },
+      ColorColumn { x = 90 },
+      ColorColumn { x = 100 },
+    }
+  }
+end)
+
+LuactLove.init(Love.Fragment {
+  App { x = 500, y = 0 },
+  App { x = 600, y = 0 },
+  App { x = 500, y = 100 },
+  App { x = 600, y = 100 },
+})
